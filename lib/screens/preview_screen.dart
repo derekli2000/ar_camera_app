f7a0816ba8029b93597b9b4082c67961c5fde28b
Derek Li <lid19@rpi.edu>
2020-05-12 21:43:17 -0400

    Fixed UI elements for the capture preview screen

--- lib/screens/preview_screen.dart
+++ lib/screens/preview_screen.dart
@@ -2,8 +2,11 @@ import 'dart:io';
 import 'package:arcameraapp/models/SecureStoreMixin.dart';
 import 'package:arcameraapp/screens/camera_screen.dart';
 import 'package:arcameraapp/widgets/zoomable_item.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:arcameraapp/services/https_requests.dart';
+
+import 'file_picker.dart';
 
 class PreviewImageScreen extends StatefulWidget with SecureStoreMixin {
   final String imagePath;
@@ -21,7 +24,6 @@ class PreviewImageScreen extends StatefulWidget with SecureStoreMixin {
 class _PreviewImageScreenState extends State<PreviewImageScreen> {
   HttpRequests httpRequests = new HttpRequests();
   GlobalKey _filePosition = new GlobalKey();
-  GlobalKey _capturePosition = new GlobalKey();
 
   _confirmSend(BuildContext context, String imagePath, String sharedImagePath) {
     return showDialog(
@@ -48,8 +50,8 @@ class _PreviewImageScreenState extends State<PreviewImageScreen> {
                 Navigator.pushAndRemoveUntil(
                     context,
                     MaterialPageRoute(
-                        builder: (BuildContext context) => CameraScreen()),
-                    ModalRoute.withName('/CameraScreen'));
+                        builder: (BuildContext context) => FilePickerPage()),
+                    ModalRoute.withName('/FilePickerPage'));
               },
             )
           ],
@@ -57,93 +59,63 @@ class _PreviewImageScreenState extends State<PreviewImageScreen> {
   }
 
   _getPositions() {
-    final RenderBox filePositionBox = _filePosition.currentContext.findRenderObject();
+    final RenderBox filePositionBox =
+        _filePosition.currentContext.findRenderObject();
     final positionFile = filePositionBox.localToGlobal(Offset.zero);
-    final RenderBox capturePos = _capturePosition.currentContext.findRenderObject();
-    final positionCapture = capturePos.localToGlobal(Offset.zero);
-    print("width: ${capturePos.size}");
-    print("POSITION of File: $positionFile, POSITION OF CAPTURE: $positionCapture ");
+    print("POSITION of File: $positionFile");
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        iconTheme: Theme.of(context).iconTheme,
-      ),
-      body: Container(
-        child: SafeArea(
-          child: Stack(children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    child: Stack(
-                      children: <Widget>[
-                        Container(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0),
-                              key: _capturePosition,
-                              child: Image.file(
-                                File(widget.imagePath),
-                                fit: BoxFit.fill,
-                                width: MediaQuery.of(context).size.width,
-                              ),
-                            ),
-                          ),
-                        ),
-                        ZoomableItem(
-                          child: Container(
-                            width: MediaQuery.of(context).size.width,
-                            height: MediaQuery.of(context).size.height,
-                            key: _filePosition,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0),
-                              child: Image.file(
-                                File(widget.sharedImagePath),
-                                scale: 10.0,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: FloatingActionButton(
-                          child: Icon(Icons.share),
-                          onPressed: () {
-                            print("pressed send");
-                            _getPositions();
-                            _confirmSend(context, widget.imagePath,
-                                widget.sharedImagePath);
-                          }),
-                    )
-                  ],
-                ),
-                SizedBox(height: 20.0)
-              ],
-            ),
-//              Container(
-//                child: MovableStackItem(filePath: widget.sharedImagePath)
-          ]),
-        ),
-      ),
+    Image shareImg = Image.file(
+      File(widget.sharedImagePath),
+      scale: 10.0,
+      fit: BoxFit.cover,
     );
+
+    return Stack(children: <Widget>[
+      Image.file(
+        File(widget.imagePath),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        fit: BoxFit.cover,
+      ),
+      Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          iconTheme: Theme.of(context).iconTheme,
+        ),
+        backgroundColor: Colors.transparent,
+
+        body: Stack(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: ZoomableItem(
+                child: Container(
+                    child: OverflowBox(
+                        minWidth: 0.0,
+                        minHeight: 0.0,
+                        maxWidth: double.infinity,
+                        child: ClipRRect(
+                            key: _filePosition,
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: shareImg))),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.share),
+            onPressed: () {
+              print("pressed send");
+              _getPositions();
+              _confirmSend(
+                  context, widget.imagePath, widget.sharedImagePath);
+            }),
+      ),
+    ]);
   }
 }
